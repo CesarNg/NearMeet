1836073f4190b0bbb1368a5f73eb451fb481c1ce
LaureBedu6 <bedu.laure@wanadoo.fr>
2018-10-26 15:25:53 -0500

    Add spashscreen + logo (drawable)

---
+++ app/src/main/java/com/hfad/nearmeet/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.hfad.nearmeet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private static int TIME_OUT = 2000;
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+
+    }
+}
+
