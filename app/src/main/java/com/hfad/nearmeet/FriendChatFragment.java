eb5e89fa47d67b735a5ee655d7f38d1f19d74b1b
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2018-11-13 22:51:09 -0500

    FriendChatFragment

---
+++ app/src/main/java/com/hfad/nearmeet/FriendChatFragment.java
@@ -0,0 +1,251 @@
+package com.hfad.nearmeet;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.hfad.nearmeet.Model.Message;
+import com.hfad.nearmeet.Model.User;
+import com.hfad.nearmeet.api.MessageHelper;
+import com.hfad.nearmeet.api.UserHelper;
+import com.hfad.nearmeet.friend_chat.FriendChatAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FriendChatFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FriendChatFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendChatFragment extends Fragment implements FriendChatAdapter.Listener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // FOR DESIGN
+    // 1 - Getting all views needed
+    @BindView(R.id.activity_friend_chat_recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.activity_friend_chat_text_view_recycler_view_empty)
+    TextView textViewRecyclerViewEmpty;
+    @BindView(R.id.activity_friend_chat_message_edit_text)
+    TextInputEditText editTextMessage;
+    @BindView(R.id.activity_friend_chat_image_chosen_preview)
+    ImageView imageViewPreview;
+
+    private com.hfad.nearmeet.friend_chat.FriendChatAdapter FriendChatAdapter;
+    @Nullable
+    private User modelCurrentUser;
+    private String currentChatName;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // STATIC DATA FOR CHAT (3)
+    private static final String CHAT_NAME_ANDROID = "android";
+    private static final String CHAT_NAME_BUG = "bug";
+    private static final String CHAT_NAME_FIREBASE = "firebase";
+
+    public FriendChatFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FriendChatFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FriendChatFragment newInstance(String param1, String param2) {
+        FriendChatFragment fragment = new FriendChatFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_friend_chat, container, false);
+        ButterKnife.bind(this,view);
+        this.configureRecyclerView(CHAT_NAME_ANDROID);
+        // this.configureToolbar();
+        this.getCurrentUserFromFirestore();
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+       /* if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @OnClick(R.id.activity_friend_chat_send_button)
+    public void onClickSendMessage() {
+
+        // 1 - Check if text field is not empty and current user properly downloaded from Firestore
+        if (!TextUtils.isEmpty(editTextMessage.getText()) && modelCurrentUser != null) {
+            // 2 - Create a new Message to Firestore
+            MessageHelper.createMessageForChat(editTextMessage.getText().toString(), this.currentChatName, modelCurrentUser).addOnFailureListener(this.onFailureListener());
+            // 3 - Reset text field
+            this.editTextMessage.setText("");
+
+        }
+    }
+
+    @OnClick({ R.id.activity_friend_chat_android_chat_button, R.id.activity_friend_chat_firebase_chat_button, R.id.activity_friend_chat_bug_chat_button})
+    public void onClickChatButtons(ImageButton imageButton) {
+        // 8 - Re-Configure the RecyclerView depending chosen chat
+        switch (Integer.valueOf(imageButton.getTag().toString())){
+            case 10:
+                this.configureRecyclerView(CHAT_NAME_ANDROID);
+                break;
+            case 20:
+                this.configureRecyclerView(CHAT_NAME_FIREBASE);
+                break;
+            case 30:
+                this.configureRecyclerView(CHAT_NAME_BUG);
+                break;
+        }
+    }
+
+    @OnClick(R.id.activity_friend_chat_add_file_button)
+    public void onClickAddFile() { }
+
+    // --------------------
+    // REST REQUESTS
+    // --------------------
+    // 4 - Get Current User from Firestore
+    private void getCurrentUserFromFirestore(){
+        UserHelper.getUser(getCurrentUser().getUid()).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                modelCurrentUser = documentSnapshot.toObject(User.class);
+            }
+        });
+    }
+
+    // --------------------
+    // UI
+    // --------------------
+    // 5 - Configure RecyclerView with a Query
+    private void configureRecyclerView(String chatName){
+        //Track current chat name
+        this.currentChatName = chatName;
+        //Configure Adapter & RecyclerView
+        this.FriendChatAdapter = new FriendChatAdapter(generateOptionsForAdapter(MessageHelper.getAllMessageForChat(this.currentChatName)), Glide.with(this), this, getCurrentUser().getUid());
+        FriendChatAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                recyclerView.smoothScrollToPosition(FriendChatAdapter.getItemCount()); // Scroll to bottom on new messages
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(this.FriendChatAdapter);
+    }
+
+    // 6 - Create options for RecyclerView from a Query
+    private FirestoreRecyclerOptions<Message> generateOptionsForAdapter(Query query){
+        return new FirestoreRecyclerOptions.Builder<Message>()
+                .setQuery(query, Message.class)
+                .setLifecycleOwner(this)
+                .build();
+    }
+
+    @Override
+    public void onDataChanged() {
+        textViewRecyclerViewEmpty.setVisibility(this.FriendChatAdapter.getItemCount() == 0 ? View.VISIBLE : View.GONE);
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public OnFailureListener onFailureListener(){
+        return new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    @Nullable
+    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+}
